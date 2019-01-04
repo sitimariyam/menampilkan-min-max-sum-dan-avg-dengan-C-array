3c4fa291876acd945746b74c0a7c298e97b35c50
mariyammayy <45526379+sitimariyam@users.noreply.github.com>
2019-01-05 05:24:02 +0700

    Add files via upload

---
+++ percobaan kembali array.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+float n,min,max,bil,i,jum,rata2;
+int pilih;
+
+
+cout<<"MENGHITUNG NILAI JUMLAH, RATA-RATA, MAKSIMAL & MINIMAL "<<endl;
+
+cout<<"Masukkan jumlah bilangan : "; cin>>n;
+jum=0;
+min=100;
+max=0;
+for(i=1;i<=n;i++){
+    cout<<"Masukan N ke-"<<i<<" : "; cin>>bil;
+  jum=jum+bil;
+   if (max<bil){
+       max=bil;
+   } else if(min>bil){
+       min=bil;
+   }
+   rata2=jum/n;
+}
+cout<<endl;
+
+menu:
+cout<<endl;
+cout<<"Pilihan : "<<endl;
+    cout<<"1. Nilai Jumlah "<<endl;
+    cout<<"2. Nilai Rata-rata "<<endl;
+    cout<<"3. Nilai Max "<<endl;
+    cout<<"4. Nilai Min "<<endl;
+    cout<<"Pilih hasil mana yang pingin anda ketahui : ";
+    cin>>pilih;
+
+ switch(pilih){
+  case 1:
+
+   cout<<"Nilai Jumlah : "<<jum<<endl;
+   getch();
+   goto menu;
+
+ case 2:
+  cout<<"Nilai Rata-rata : "<<rata2<<endl<<endl;
+  getch();
+  goto menu;
+
+ case 3:
+  cout<<"Nilai Maximum : "<<max;
+  getch();
+  goto menu;
+ case 4:
+  cout<<"Nilai Minimum : "<<min;
+  getch();
+  goto menu;
+
+ }
+
+
+
+return 0;
+
+}
